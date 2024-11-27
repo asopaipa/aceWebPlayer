9cca34ffe1d23b4e5bc45f498afdea7183c2d800
asopaipa <eslounicoquese@gmail.com>
2024-11-27 18:50:54 +0100

    Create run_docker.sh

---
+++ run_docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Pedir el puerto al usuario
+read -p "¿Qué puerto quieres usar? 80 sería una buena elección: " PORT
+
+# Preguntar si se quiere permitir el acceso remoto
+read -p "¿Quieres permitir el acceso a través de Internet? (sí/no): " ALLOW_REMOTE_ACCESS
+
+# Exportar la variable de entorno para el puerto
+export PORT=$PORT
+
+# Eliminar los comentarios (quitar "#") de las líneas correspondientes si la respuesta es "sí"
+if [ "$ALLOW_REMOTE_ACCESS" == "sí" ] || [ "$ALLOW_REMOTE_ACCESS" == "si" ]; then
+  # Si la respuesta es sí, descomentar las líneas relacionadas con ALLOW_REMOTE_ACCESS
+  echo "Configurando acceso remoto..."
+
+  # Descomentar las líneas relacionadas con environment y ALLOW_REMOTE_ACCESS
+  sed -i '/#environment:/s/#//g' docker-compose.yml
+  sed -i '/#  - ALLOW_REMOTE_ACCESS=yes/s/#//g' docker-compose.yml
+else
+  # Si la respuesta es no, no hacer cambios
+  echo "No se habilita el acceso remoto."
+fi
+
+
+
+
+docker build -t acestream-player .
+
+# Levantar el contenedor con docker-compose
+docker-compose up -d
