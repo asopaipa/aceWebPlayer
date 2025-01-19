45f6c2e8164f0d5aa0c01cad73b07ca4ad62e7d3
asopaipa <eslounicoquese@gmail.com>
2025-01-19 22:31:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, Response
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, Response, abort, safe_join, send_file
 from getLinks import generar_m3u_from_url, decode_default_url
 import re
 import os
@@ -12,6 +12,8 @@ import io
 import threading
 import time
 import shutil
+from pathlib import Path
+
 
 app = Flask(__name__)
 
@@ -423,6 +425,66 @@ def procesar_peliculas(m3u_peliculas, directorio_salida):
                 archivo_strm = os.path.join(pelicula_actual, f"{titulo} - {calidad}.strm")
                 with open(archivo_strm, "w", encoding="utf-8") as f:
                     f.write(linea)
+
+
+FolderPath = r"output_strm"
+
+
+
+def getReadableByteSize(num, suffix='B') -> str:
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Y', suffix)
+
+def getTimeStampString(tSec: float) -> str:
+    tObj = datetime.fromtimestamp(tSec)
+    tStr = tObj.strftime('%d-%m-%Y %H:%M:%S')
+    return tStr
+
+def getIconClassForFilename(fName):
+    fileExt = Path(fName).suffix
+    fileExt = fileExt[1:] if fileExt.startswith(".") else fileExt
+    fileTypes = ["aac", "ai", "bmp", "cs", "css", "csv", "doc", "docx", "exe", "gif", "heic", "html", "java", "jpg", "js", "json", "jsx", "key", "m4p", "md", "mdx", "mov", "mp3",
+                 "mp4", "otf", "pdf", "php", "png", "pptx", "psd", "py", "raw", "rb", "sass", "scss", "sh", "sql", "svg", "tiff", "tsx", "ttf", "txt", "wav", "woff", "xlsx", "xml", "yml"]
+    fileIconClass = f"bi bi-filetype-{fileExt}" if fileExt in fileTypes else "bi bi-file-earmark"
+    return fileIconClass
+
+# route handler
+@app.route('/reports/', defaults={'reqPath': ''})
+@app.route('/reports/<path:reqPath>')
+def getFiles(reqPath):
+    # Join the base and the requested path
+    # could have done os.path.join, but safe_join ensures that files are not fetched from parent folders of the base folder
+    absPath = safe_join(FolderPath, reqPath)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(absPath):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(absPath):
+        return send_file(absPath)
+
+    # Show directory contents
+    def fObjFromScan(x):
+        fileStat = x.stat()
+        # return file information for rendering
+        return {'name': x.name,
+                'fIcon': "bi bi-folder-fill" if os.path.isdir(x.path) else getIconClassForFilename(x.name),
+                'relPath': os.path.relpath(x.path, FolderPath).replace("\\", "/"),
+                'mTime': getTimeStampString(fileStat.st_mtime),
+                'size': getReadableByteSize(fileStat.st_size)}
+    fileObjs = [fObjFromScan(x) for x in os.scandir(absPath)]
+    # get parent directory url
+    parentFolderPath = os.path.relpath(
+        Path(absPath).parents[0], FolderPath).replace("\\", "/")
+    return render_template('files.html.j2', data={'files': fileObjs,
+                                                 'parentFolder': parentFolderPath})
+
+
+
 
 
 if __name__ == '__main__':
