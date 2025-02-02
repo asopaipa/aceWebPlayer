9a75b71f1ce8897503a330e51783308f9627de11
asopaipa <eslounicoquese@gmail.com>
2025-02-02 18:52:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ def procesar_peliculas(m3u_peliculas, directorio_salida):
                 if titulo_match:
                     titulo_pre = titulo_match.group(1).strip()
                     titulo = titulo_pre.replace("/", "_").replace("\\", "_").replace("-", "_").replace(" ", "_")
-                    calidad = titulo_match.group(3).strip()
+                    calidad = titulo_match.group(3).strip().replace(" ", "_").replace("[", "_").replace("]", "_")
                 else:
                     titulo_pre = info
                     titulo = titulo_pre.replace("/", "_").replace("\\", "_").replace("-", "_").replace(" ", "_")
@@ -428,7 +428,7 @@ def procesar_peliculas(m3u_peliculas, directorio_salida):
         elif linea.startswith("acestream://") or linea.startswith("http"):
             # Crear el archivo STRM con el enlace
             if pelicula_actual:
-                archivo_strm = os.path.join(pelicula_actual, f"{titulo} - {calidad}.strm")
+                archivo_strm = os.path.join(pelicula_actual, f"{titulo}-{calidad}.strm")
                 with open(archivo_strm, "w", encoding="utf-8") as f:
                     f.write(f'"{linea}"')
 
