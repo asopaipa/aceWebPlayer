e5167a3d918b4aabdb6686e17801bd4c2dd310a1
asopaipa <eslounicoquese@gmail.com>
2025-04-15 16:49:51 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-if [ -z "${ACESTREAM_IMAGE}" ]; then
-  case $(uname -m) in
-    armv7l|armv7|aarch64)
-      ACESTREAM_IMAGE="futebas/acestream-engine-arm"
-      ;;
-    *)
-      ACESTREAM_IMAGE="asopaipa/acestream-http-proxy"
-      ;;
-  esac
-fi
-
-export ACESTREAM_IMAGE
-
 DOCKER_COMPOSE_CMD=""
 
 # Verificar si docker compose está disponible
@@ -29,10 +16,7 @@ fi
 # Pedir el puerto al usuario
 read -p "¿En qué puerto quieres que se publique la web? (5001) " PORT
 
-read -p "¿En qué puerto quieres que se publique el Acestream? (6878) " PORTACE
 
-# Preguntar si se quiere permitir el acceso remoto
-read -p "¿Quieres permitir el acceso a través de Internet? (sí/NO): " ALLOW_REMOTE_ACCESS
 
 # Preguntar si se quiere permitir el acceso remoto
 read -p "Si quieres proteger la web con usuario y contraseña, introduce el usuario: " USUARIO
@@ -43,25 +27,7 @@ fi
 
 # Exportar la variable de entorno para el puerto
 export PORT=$PORT
-export PORTACE=$PORTACE
 
-# Eliminar los comentarios (quitar "#") de las líneas correspondientes si la respuesta es "sí"
-if [ "$ALLOW_REMOTE_ACCESS" == "sí" ] || [ "$ALLOW_REMOTE_ACCESS" == "si" ]; then
-  # Si la respuesta es sí, descomentar las líneas relacionadas con ALLOW_REMOTE_ACCESS
-  echo "Configurando acceso remoto..."
-
-  # Descomentar las líneas relacionadas con environment y ALLOW_REMOTE_ACCESS
-  sed -i '/#environment:/s/#//g' docker-compose.yml
-  sed -i '/#  - ALLOW_REMOTE_ACCESS=yes/s/#//g' docker-compose.yml
-else
-  # Si la respuesta es no, no hacer cambios
-  echo "No se habilita el acceso remoto."
-fi
-
-if [ -n "$PORTACE" ]; then
-    sed -i "s/6878/$PORTACE/g" ./static/js/main.js
-    sed -i "s/6878/$PORTACE/g" ./getLinks.py
-fi
 
 if [ -n "$USUARIO" ]; then
     sed -i "s/USERNAME = \"\"/USERNAME = \"$USUARIO\"/g" app.py
